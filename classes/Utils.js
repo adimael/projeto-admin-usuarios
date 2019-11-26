4fad64e6372afa012d6c0a80765fe4b4b0881f9b
adimael <adimaelbr@gmail.com>
2019-11-25 23:50:01 -0300

    Projeto admin usuarios

---
+++ classes/Utils.js
@@ -0,0 +1,9 @@
+class Ultils {
+
+  static dateFormat(date){
+
+    return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes();
+
+  }
+
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+let userController = new UserController("form-user-create", "form-user-update", "table-users");
